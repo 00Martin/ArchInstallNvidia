ced5b6dd4f37fc5c47a6923d5c630385a95939f1
MD^ (Martin) <23630655+00Martin@users.noreply.github.com>
2023-04-10 00:48:35 +0200

    Initial commit of the first script

---
+++ install1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#version 0.1
+
+echo -e "\nCreate your partitions first with -> cfdisk /dev/sda\n\nYour partitions must look like this:"
+echo -e "sda1 - boot efi, under 4GB, typically 512MB-2GB, 2GB recommended to allow multiple kernels\nsda2 - system, can fill up the rest of the disk\n\n"
+
+echo "Have you done this ?[n/y]"
+read answer
+
+if [ $answer == "y" ]; then
+echo -e "\nnext\n"
+else
+echo -e "\nPlease create your partitions and start the script again\n"
+fi
