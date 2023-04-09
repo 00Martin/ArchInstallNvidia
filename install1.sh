096e5b60cf0df7c2bfa1b3ef7f28e70528289bc5
MD^ (Martin) <23630655+00Martin@users.noreply.github.com>
2023-04-10 01:15:28 +0200

    Changed y for yes to Y

--- install1.sh
+++ install1.sh
@@ -4,10 +4,10 @@
 echo -e "\nCreate your partitions first with -> cfdisk /dev/sda\n\nYour partitions must look like this:"
 echo -e "sda1 - boot efi, under 4GB, typically 512MB-2GB, 2GB recommended to allow multiple kernels\nsda2 - system, can fill up the rest of the disk\n\n"
 
-echo "Have you done this ?[n/y]"
+echo "Have you done this ? [n/Y]"
 read answer
 
-if [ $answer == "y" ]; then
+if [ $answer == "Y" ]; then
 echo -e "\nnext\n"
 else
 echo -e "\nPlease create your partitions and start the script again\n"
