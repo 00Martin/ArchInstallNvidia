64300821d3f5d19b1f73b28faa9e3b0584f00ad8
MD^ (Martin) <23630655+00Martin@users.noreply.github.com>
2023-04-10 16:24:12 +0200

    install2 creation

---
+++ install2.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#v1.0e
+
+#Set time zone to Switzerland
+ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+
+#Sychronize clocks
+hwclock --systohc
+
+
+#Set the locale
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+#Set keyboard for the system and Plasma on X11
+echo "KEYMAP=fr_CH" > /etc/vconsole.conf
+echo "XKBLAYOUT=ch" >> /etc/vconsole.conf
+echo "XKBVARIANT=fr" >> /etc/vconsole.conf
+
+
+#set hostname and hosts
+echo "martinpc" > /etc/hostname
+
